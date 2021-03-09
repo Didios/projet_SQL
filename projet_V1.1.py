0a5f708d9abae1a585fc9c7a0ea95b05152ce0d0
Didios <78910620+Didios@users.noreply.github.com>
2021-03-09 22:34:08 +0100

    documentation
    documentation des fonctions
    documentation de certaines ligne du programme
    division de certaines actions en fonctions

--- projet_V1.1.py
+++ projet_V1.1.py
@@ -84,6 +84,10 @@ def question():
 
 def ajouter(root, dico):
     """
+    fonction permettant d'ajouter une requête via une fenetre tkinter
+    parametres:
+        root, une fenetre tkinter
+        dico, un dictionnaire du type {indice de la question : [question, reponse, fichier sql associé], ... }
     """
     # on enlève ce qui se trouve sur la fenetre
     tk.clean(root, "Menu")
@@ -92,58 +96,62 @@ def ajouter(root, dico):
 
     # ensemble des tooltips de champs et des verification de champs
     def numero_verifier(event):
-        try:
-            int(numero.get("current linestart", "current lineend"))
-            renseignement[0] = True
-            # on enleve le message en rouge
-            n_texte.set("")
-        except:
+        """
+        sous-fonction qui permet de vérifier que le champs pour l'indice est correcte
+        """
+        try: # on tente d'obtenir un entier avec le contenu du champs
+            nbr = int(numero.get("current linestart", "current lineend"))
+            if nbr in dico.keys():
+                renseignement[0] = False
+                n_texte.set("L'indice indiqué existe déjà")
+            else:
+                renseignement[0] = True
+                if nbr < 10: # on fait en sorte de toujours écrire les nombres avec 2 chiffres
+                    numero.delete("current linestart", "current lineend")
+                    numero.insert("current linestart", "0"+ str(nbr))
+                n_texte.set("")
+        except: # si ca ne marche pas, les conditions ne sont pas remplis : erreur
             renseignement[0] = False
-            # on met le message en rouge
             n_texte.set("L'indice de la requetes doit être un entier")
 
     def question_verifier(event):
-        texte = question_sql.get("current linestart", "current lineend")
-        lettres = []
+        """
+        sous-fonction qui permet de vérifier que le champs pour la question est correcte
+        """
+        texte = question_sql.get("current linestart", "current lineend") # on prend la question du premier au dernier caracteres de la ligne
 
-        for caractere in texte:
-            if caractere not in lettres:
-                lettres += [caractere]
+        lettres = caracteres(texte) # on liste les caracteres du champs pour voir si il y a quelque chose d'écrit (autre que des espaces)
 
-        if texte == "" or ( len(lettres) == 1 and lettres[0] == " "):
+        if texte == "" or ( len(lettres) == 1 and lettres[0] == " "): # On regarde si le champs n'est pas vide
             renseignement[1] = False
-            # on met le message en rouge
             q_texte.set("La requete doit posséder une question/sujet/énoncé")
         else:
             renseignement[1] = True
-            # on enleve le message en rouge
             q_texte.set("")
 
     def requete_verifier(event):
-        texte = requete.get("1.0", "end")
-        lettres = []
-        for caractere in texte:
-            if caractere not in lettres:
-                lettres += [caractere]
+        """
+        sous-fonction qui permet de vérifier que le champs pour la requete est correcte
+        """
+        texte = requete.get("1.0", "end") # on prend la requete du premier caracteres jusqu'au dernier
+        lettres = caracteres(texte)
 
-        data = Database.database("imdb.db")
-        if data.execute(texte) == None:
+        data = Database.database("imdb.db") # on met en route la base de données pour tester
+
+        if data.execute(texte) == None: # si on obtient None, la requete ne s'est pas exécuté est donc elle est fausse
             renseignement[2] = False
-            # on met le message en rouge
             r_texte.set("La requete n'est pas valide, veuillez corriger")
-        elif len(lettres) < 3 and (" " in lettres or "\n" in lettres):
+        elif len(lettres) < 3 and (" " in lettres or "\n" in lettres): # si la requete ne contient que des espaces, elle peut s'éxécuter, ce cas est donc détaillé
             renseignement[2] = False
-            # on met le message en rouge
             r_texte.set("Veuillez écrire une requête")
         else:
             renseignement[2] = True
-            # on enleve le message en rouge
             r_texte.set("")
 
 
     def incrementation():
         """
-        fonction permettant le remplissage automatique du champs indice de la requetes
+        sous-fonction permettant le remplissage automatique du champs 'indice de la requete'
         """
         indice = [t for t in dico.keys()]
         i_max = max(indice)
@@ -152,11 +160,15 @@ def ajouter(root, dico):
         numero.insert("insert", str(i_max + 1))
 
     def validation():
-
+        """
+        sous-fonction permettant de faire la validation des conditions et d'ajouter la requête au fichier
+        """
+        # on effectue une vérification pour chaque champs
         numero_verifier("valider")
         question_verifier("valider")
         requete_verifier("valider")
 
+        # on affiche une erreur si une condition n'est pas remplie
         if renseignement[0] == False: # l'indice indiquer ne contient autre chose que des chiffres ou est vide
             messagebox.showerror("Indice", "Seules les chiffres sont autorisées pour les indices. Aucun espace, aucune lettres ou caractère spécial n'est autorisé")
 
@@ -167,31 +179,32 @@ def ajouter(root, dico):
             messagebox.showerror("Requête SQL", "La requête SQL n'est pas fonctionelle, veuillez corriger votre requete")
 
         else:
-            index = str(numero.get("current linestart", "current lineend"))
+            index = numero.get("current linestart", "current lineend")
 
             texte_requete = requete.get("1.0", "end")
             question_sujet = "#" + index + "." + question_sql.get("1.0", "end")
-            sql_file = "req" + index + ".sql"
+            sql_file = "req" + str(int(index)) + ".sql" # le str(int permet de transformer les nombre du type 01 en 1
 
-            # si tous est bon :
-            with open("requetes/" + sql_file, "x") as fichier: # on cree un fichier .sql pour ajouter la requete
+            with open("requetes/" + sql_file, "x") as fichier: # on cree un fichier .sql en écriture pour ajouter la requete
                 fichier.write(texte_requete)
 
             with open("requetes/alire.md", "a") as alire: # on ajoute les données de la requete dans alire.md
                 alire.write("\n \n" + sql_file + "\n" + question_sujet + texte_requete)
 
             retour() # on retourne sur l'écran principal
-            # faire en sorte de ne pas fermer la fenetre = changement de question -> initialisation + base
 
     def retour():
+        """
+        sous-fonction permettant de revenir au menu principal
+        elle fait redémarrer le programme
+        """
         root.destroy()
-        question() # on retourne sur l'écran principal
+        question() # on relance le programme
 
     # affichage pour la demande de l'indice
     Tk.Label(root, text = "Indice de la question :").grid(row = 0, column = 0, sticky = "w")
     Tk.Button(root, text = "Incrémentation automatique", command = incrementation).grid(row = 0, column = 1, sticky = "e")
     numero =  Tk.Text(root, height = 1, width = 100)
-    numero.bind("<Enter>", numero_verifier)
     numero.bind("<Leave>", numero_verifier)
     numero.grid(row = 1, column = 0, columnspan = 2)
 
@@ -203,7 +216,6 @@ def ajouter(root, dico):
     # affichage pour la demande de la question
     Tk.Label(root, text = "Question posée : ").grid(row = 3, column = 0, sticky = "w")
     question_sql = Tk.Text(root, height = 1, width = 100)
-    question_sql.bind("<Enter>", question_verifier)
     question_sql.bind("<Leave>", question_verifier)
     question_sql.grid(row = 4, column = 0, columnspan = 2)
 
@@ -212,12 +224,13 @@ def ajouter(root, dico):
     erreur_question = Tk.Label(root, text = "", fg = "red", textvariable = q_texte)
     erreur_question.grid(row = 5, column = 0, sticky = "w")
 
-    # affichage pour la demande de la requêtes
+    # affichage pour la demande de la requête
     Tk.Label(root, text = "Requete SQL :").grid(row = 6, column = 0, sticky = "w")
     requete = Tk.Text(root, height = 10, width = 100)
+    requete.bind("<Leave>", requete_verifier)
     requete.grid(row = 7, column = 0, columnspan = 2)
 
-    # affichage en cas de problemes avec la requetes
+    # affichage en cas de problemes avec la requete
     r_texte = Tk.StringVar()
     erreur_requete = Tk.Label(root, text = "", fg = "red", textvariable = r_texte)
     erreur_requete.grid(row = 8, column = 0, sticky = "w")
@@ -226,54 +239,95 @@ def ajouter(root, dico):
     Tk.Button(root, text = "valider", command = validation, width = 20, height = 5).grid(row = 9, column = 0, sticky = "w")
     Tk.Button(root, text = "retour", command = retour, width = 20, height = 5).grid(row = 9, column = 1, sticky = "e")
 
+def caracteres(texte):
+    """
+    fonction permettant d'obtenir une liste de tous les caracteres distinct présent dans une chaine de caracteres
+    parametres:
+        texte, une chaine de caracteres
+    renvoie une liste de caracteres
+    """
+    lettres = []
+
+    for caractere in texte:
+        if caractere not in lettres:
+            lettres += [caractere]
+
+    return lettres
+
 def supprimer(root, dico):
     """
+    fonction permettant de supprimer une requête via une fenetre tkinter
+    parametres:
+        root, une fenetre tkinter
+        dico, un dictionnaire du type {indice de la question : [question, reponse, fichier sql associé], ... }
     """
     # on nettoie la fenêtre et on change son nom
     tk.clean(root, "Menu")
     root.title("Suppression de requêtes")
     questions_supprimer = []
 
-    def validation():
-        suppr = []
+    def cases_cochees():
+        """
+        sous-fonction permettant de connaître les cases qui ont été coché
+        (questions_supprimer est une liste de variables, donc les valeurs changent sans intervention dans le programme)
+        renvoi une liste des index des cases sélectionner
+        """
+        numeros_cases = []
+
         for i in range(0,len(questions_supprimer)):
             if questions_supprimer[i][0].get() == 1: # On regarde si la case à été coché
-                suppr.append(questions_supprimer[i][1]) # on ajoute l'index de la question
+                numeros_cases.append(questions_supprimer[i][1]) # on ajoute l'index de la question
+
+        return numeros_cases
+
+    def validation():
+        """
+        sous-fonction permettant de :
+            connaître les cases cochées
+            retirer les requetes associée aux cases cochées
+        """
+        suppr = cases_cochees()
 
         if messagebox.askyesno("Validation", "Est-tu sûr de vouloir supprimer (pour toujours) les questions :" + str(suppr)):
-            # cherche les lignes de la premiere questions
-            # les supprimer
-            # recommencer avec la suivante
-            alire =[0] + read.lire_fichier("requetes/alire.md", True)
+            alire =[0] + read.lire_fichier("requetes/alire.md", True) # l'ajout de la premiere valeur permet d'être en accrod sur le décompte avec la fonction read.suppr_lignes
 
-            i_lignes = []
+            i_lignes = [] # on va stocker ici la liste des lignes à supprimer du document alire.md
 
-            for n_question in suppr:
+            for n_question in suppr: # on parcours chaque questions sélectionner
 
+                # on avance le compteur l des indices tant qu'on n'est pas sur la bonne question
                 l = 1
                 while alire[l][:3] != n_question:
                     l += 1
 
-
+                # on supprime le fichier sql associé qui se trouve une ligne au dessus
                 read.suppr_fichier("requetes/" + alire[l-1], False)
                 i_lignes += [l-1]
 
+                # la détection se fait par la précence de # donc on enleve manuellement le premier
                 i_lignes += [l]
                 l += 1
 
+                # on ajoute les indices tant qu'on n'as pas fini la question
                 while l < len(alire) and len(alire[l]) > 0 and alire[l][0] != "#":
                     i_lignes += [l]
                     l += 1
 
-            read.suppr_lignes("requetes/alire.md", *i_lignes)
+            read.suppr_lignes("requetes/alire.md", *i_lignes) # une fois tous les indices de lignes récoltées, on supprime les lignes
 
-            retour()
+            retour() # on retourne au menu
 
     def retour():
-        root.destroy()
-        question() # on retourne sur l'écran principal
+        """
+        sous-fonction permettant de revenir au menu principal
+        elle fait redémarrer le programme
+        """
+        root.destroy() # on ferme la fenêtre
+        question() # on relance le programme
+        # cette méthode permet de rafraichir le dictionnaire contenant les informations des requetes
 
 
+    # d'ici à la barre, on installe une scrollbar pour pouvoir afficher toutes les questions (cela évite les fenêtres trop grandes)
     ff = Tk.Frame(root)
     ff.grid(row=0, column=0, columnspan=2)
 
@@ -291,13 +345,15 @@ def supprimer(root, dico):
     vbar.config(command=canvas.yview)
     canvas['yscrollcommand']=vbar.set
     canvas.create_window((0,0),window=frame,width=W, height=H, anchor='nw')
+    ############################################################################
 
     # on ajoute les requêtes
     ligne = 0
-    for number, lien in dico.items():
+    for number, lien in dico.items(): # on parcours le dictionnaire qui contient toutes les questions
         valeur = Tk.IntVar()
-        check = Tk.Checkbutton(frame, text = dico[number][0], variable = valeur)
+        check = Tk.Checkbutton(frame, text = dico[number][0], variable = valeur) # mettre une variable permet de ne pas faire une fonction pour chaque cases pour voir si elle est coché ou non lors de la suppression
         ligne += 1
+        # les questions à un chiffre on un 0 devant elles (req1.sql => #01), ils faut donc les différencier afin d'éviter les erreurs
         if number < 10:
             questions_supprimer.append([valeur, "#0" + str(number)])
         else:
@@ -349,7 +405,7 @@ def credit():
 def test():
     """
     fonction permettant de tester l'intégralité des requêtes recensées dans alire.md
-    renvoie une série de print qui indique le résultat du test pour chaque requête
+    renvoie un print indiquant les résultats des tests
     """
     tout_juste = True
     liste_echec = []
@@ -360,7 +416,7 @@ def test():
 
         try: # On teste si on peut en effectuer la requete
             resultat = read.execute_sql_file("requetes", dictionnaire[choix][2], "imdb.db")
-        except:
+        except: # sinon cela signifie que tous n'est pas juste et on ajoute le numero de la requetes dans les echec
             tout_juste = False
             liste_echec += [choix]
 
