b68bef526a1712a9275da9dced3978da4b3456c2
Didios <78910620+Didios@users.noreply.github.com>
2021-03-19 19:57:10 +0100

    transformation
    transformation de tkinter vers html
    possibilité de lancer une page avec les credits/l'aide
    ajout d'un menu
    les boutons ouvrent désormais un nouvel onglet quand ils sont cliqué
    mise en ordre des tests du fichier test.html

--- projet_V1.0_html.py
+++ projet_V1.0_html.py
@@ -1,5 +1,7 @@
 #!"C:\Winpython\python-3.8.5.amd64\python.exe"
 
+# programme réalisé par Didier Mathias en classe de Terminale B
+
 # importation des modules necessaires
 import os.path
 from modules import module_database as Database
@@ -63,30 +65,27 @@ def Lancement(base, requetes, config):
         dictionnaire = stockage_question("requetes", "alire.md") # On stocke toutes les informations contenu dans le fichier de configuration
         supprimer(dictionnaire, "requetes", "alire.md", base)
 
-    """
-    # On crée un Menu pour l'aide et les crédits
-    barremenu = Tk.Menu(root)
+    credit()
+    aide()
 
-    barre_aide = Tk.Menu(barremenu, tearoff=0)
-    barre_modification = Tk.Menu(barremenu, tearoff=0)
+    pprint.debuthtml("Questions :")
 
-    # On ajoute les deux sous-menus : aide et credits
-    barre_aide.add_command(label="Aide", underline=0, command = aide) # la fonction aide sert à mettre des parametres en dérivation, puisque directement, cela ne fonctionne pas
-    barre_aide.add_command(label="Crédits", underline=0, command = credit) # la fonction credit sert à mettre des parametres en dérivation, puisque directement, cela ne fonctionne pas
+    print('<menu type="toolbar">')
+    print('<menu label="A propos">')
+    print('<button onclick="window.open(%s)"> %s </button>' % ("'data/credits.html'", "Crédits"))
+    print('<button onclick="window.open(%s)"> %s </button>' % ("'data/aide.html'", "Aide"))
+    print("</menu>")
+    print("<menu>")
 
-    # on ajoute les sous menus ajout et suppression
-    barre_modification.add_command(label="Ajout", underline=0, command = add)
-    barre_modification.add_command(label="Suppression", underline=0, command = suppr)
-
-    # On place les sous-menus crée dans le Menu barremenu
-    barremenu.add_cascade(label="A propos", underline=0, menu=barre_aide)
-    barremenu.add_cascade(label="Modification", underline=0, menu=barre_modification)
-
-    root.config(menu=barremenu) # On place le menu dans la fenetre
-    """
+    # menu avec bouton :                            permanents
+    #    aide
+    #    credits
+    #    ajout
+    #    suppression
 
     question(requetes, config, base)
 
+    # raccourci clavier
 """
     # ajout de raccourci clavier
     root.bind("<Control-KeyPress-a>", add)
@@ -104,20 +103,20 @@ def question(requetes, config, base, dictionnaire = None):
         dictionnaire, optionnel, un dictionnaire indiquant les informations des requetes comme suit : {indice de la question : [question, reponse, fichier sql associé], ... }
     affiche les questions dans le fenetre tkinter, la selection de la question entraine l'affichage de la réponse dans une autre fenetre tkinter
     """
-    pprint.debuthtml("Projet SQL")
 
     if dictionnaire is None:
         dictionnaire = stockage_question(requetes, config) # On stocke toutes les informations contenu dans le fichier de configuration
 
-    pprint.affichage_question_html(dictionnaire, base) # On lance l'affichage des questions dans une fenetre tkinter
+    pprint.affichage_question_html(dictionnaire, base)
 
     pprint.finhtml()
 
     pprint.affichage_question_html(dictionnaire, base, True)
 
+# c'est une sorte de div qui s'ajoute quand on clique sur le bouton
 def ajouter(root, dico, requetes, config, base):
     """
-    fonction permettant d'ajouter une requête via une fenetre tkinter
+    fonction permettant d'ajouter une requête via une #############################################
     parametres:
         root, une fenetre tkinter
         dico, un dictionnaire du type : {indice de la question : [question, reponse, fichier sql associé], ... }
@@ -125,65 +124,27 @@ def ajouter(root, dico, requetes, config, base):
         config, une chaine de caracteres avec le nom du fichier de configuration des requetes
         base, une chaine de caracteres avec le chemin d'accès vers la base de données
     """
-    # on enlève ce qui se trouve sur la fenetre
-    tk.clean(root, "Menu")
-    root.title("Ajout de requête")
-    renseignement = [False, False, False]
+    # on affiche le div qui contient des champs à remplir
+    # ou on mets des boites de dialogue en route qui receuille les informations
 
     # ensemble des tooltips de champs et des verification de champs
     def numero_verifier(event):
         """
         sous-fonction qui permet de vérifier que le champs pour l'indice est correcte
         """
-        try: # on tente d'obtenir un entier avec le contenu du champs
-            nbr = int(numero.get("current linestart", "current lineend"))
-            if nbr in dico.keys():
-                renseignement[0] = False
-                n_texte.set("L'indice indiqué existe déjà")
-            else:
-                renseignement[0] = True
-                if nbr < 10: # on fait en sorte de toujours écrire les nombres avec 2 chiffres minimum : 1 -> 01
-                    numero.delete("current linestart", "current lineend")
-                    numero.insert("current linestart", "0"+ str(nbr))
-                n_texte.set("")
-        except: # si ca ne marche pas, les conditions ne sont pas remplis : erreur
-            renseignement[0] = False
-            n_texte.set("L'indice de la requetes doit être un entier")
+        pass
 
     def question_verifier(event):
         """
         sous-fonction qui permet de vérifier que le champs pour la question est correcte
         """
-        texte = question_sql.get("current linestart", "current lineend") # on prend la question du premier au dernier caracteres de la ligne
-
-        lettres = caracteres(texte) # on liste les caracteres du champs pour voir si il y a quelque chose d'écrit (autre que des espaces)
-
-        if texte == "" or ( len(lettres) == 1 and lettres[0] == " "): # On regarde si le champs n'est pas vide
-            renseignement[1] = False
-            q_texte.set("La requete doit posséder une question/sujet/énoncé")
-        else:
-            renseignement[1] = True
-            q_texte.set("")
+        pass
 
     def requete_verifier(event):
         """
         sous-fonction qui permet de vérifier que le champs pour la requete est correcte
         """
-        texte = requete.get("1.0", "end") # on prend la requete du premier caracteres jusqu'au dernier
-        lettres = caracteres(texte)
-
-        data = Database.database(base) # on met en route la base de données pour tester
-
-        if data.execute(texte) == None: # si on obtient None, la requete ne s'est pas exécuté est donc elle est fausse
-            renseignement[2] = False
-            r_texte.set("La requete n'est pas valide, veuillez corriger")
-        elif len(lettres) < 3 and (" " in lettres or "\n" in lettres): # si la requete ne contient que des espaces, elle peut s'éxécuter, ce cas est donc détaillé
-            renseignement[2] = False
-            r_texte.set("Veuillez écrire une requête")
-        else:
-            renseignement[2] = True
-            r_texte.set("")
-
+        pass
 
     def incrementation():
         """
@@ -192,127 +153,38 @@ def ajouter(root, dico, requetes, config, base):
         indice = [t for t in dico.keys()]
         i_max = max(indice)
 
-        numero.delete("1.0", "end")
-        numero.insert("insert", str(i_max + 1))
-
     def validation():
         """
         sous-fonction permettant de faire la validation des conditions et d'ajouter la requête au fichier
         """
-        # on effectue une vérification pour chaque champs
-        numero_verifier("valider")
-        question_verifier("valider")
-        requete_verifier("valider")
-
-        # on affiche une erreur si une condition n'est pas remplie
-        if renseignement[0] == False: # l'indice indiquer ne contient autre chose que des chiffres ou est vide
-            messagebox.showerror("Indice", "Seules les chiffres sont autorisées pour les indices. Aucun espace, aucune lettres ou caractère spécial n'est autorisé")
-
-        elif renseignement[1] == False: # la question est inexistante
-            messagebox.showerror("Question", "Un énoncé, un sujet ou une question doit être fourni pour toutes requête ajouté")
-
-        elif renseignement[2] == False: # la requete n'est pas bonne
-            messagebox.showerror("Requête SQL", "La requête SQL n'est pas fonctionelle, veuillez corriger votre requete")
-
-        else:
-            read.suppr_fichier(requetes + "\temp.txt", False)
-
-            index = numero.get("current linestart", "current lineend")
-
-            texte_requete = requete.get("1.0", "end")
-            question_sujet = "#" + index + "." + question_sql.get("1.0", "end")
-            sql_file = "req" + str(int(index)) + ".sql" # le str(int permet de transformer les nombre du type 01 en 1
-
-            read.add_fichier(requetes, sql_file, texte_requete) # on cree un fichier .sql en écriture pour ajouter la requete
-
-            read.add_ligne(requete, config, "\n \n" + sql_file + "\n" + question_sujet + texte_requete) # on ajoute les données de la requete dans le fichier de configuration
-
-            retour() # on retourne sur l'écran principal
-
-    def retour(event = ""):
-        """
-        sous-fonction permettant de revenir au menu principal
-        """
-        question(root, requetes, config, base)
+        pass
 
     def temp():
         """
         sous-fonction qui permet de gérer un fichier temporaire qui contiendrat les informations en cours d'enregistrement
         cette sous-fonction est une boucle
         """
-        try: # on essaye d'enregistrer les informations
-            read.remplacer_ligne(requetes, "temp.txt", 1, numero.get("current linestart", "current lineend"))
-            read.remplacer_ligne(requetes, "temp.txt", 2, question_sql.get("current linestart", "current lineend"))
-
-            # on efface toutes les autres lignes pour mettre la requete
-            temporaire = read.lire_fichier(requetes + "/temp.txt", True)
-            read.suppr_lignes(requetes + "/temp.txt", * [i for i in range(3, len(temporaire) +1)])
-
-            read.remplacer_ligne(requetes, "temp.txt", 3, requete.get("1.0", "end")) # on ajoute la requete en cours au fichier
-
-            root.after(100, temp) # On répète la fonction toutes les 0.1s
-        except: # si on ne peut pas, cela signifie qu'il n'y a plus de fentre et donc la fonction s'arrête
-            pass
+        pass
 
     # affichage pour la demande de l'indice
-    Tk.Label(root, text = "Indice de la question :").grid(row = 0, column = 0, sticky = "w")
-    Tk.Button(root, text = "Incrémentation automatique", command = incrementation).grid(row = 0, column = 1, sticky = "e")
-    numero =  Tk.Text(root, height = 1, width = 100)
-    numero.bind("<Leave>", numero_verifier)
-    numero.grid(row = 1, column = 0, columnspan = 2)
-
     # affichage en cas d'erreur d'indice
-    n_texte = Tk.StringVar()
-    erreur_numero =  Tk.Label(root, text = "", fg = "red", textvariable = n_texte)
-    erreur_numero.grid(row = 2, column = 0, sticky = "w")
 
     # affichage pour la demande de la question
-    Tk.Label(root, text = "Question posée : ").grid(row = 3, column = 0, sticky = "w")
-    question_sql = Tk.Text(root, height = 1, width = 100)
-    question_sql.bind("<Leave>", question_verifier)
-    question_sql.grid(row = 4, column = 0, columnspan = 2)
-
     # affichage en cas d'abcence de question
-    q_texte = Tk.StringVar()
-    erreur_question = Tk.Label(root, text = "", fg = "red", textvariable = q_texte)
-    erreur_question.grid(row = 5, column = 0, sticky = "w")
 
     # affichage pour la demande de la requête
-    Tk.Label(root, text = "Requete SQL :").grid(row = 6, column = 0, sticky = "w")
-    requete = Tk.Text(root, height = 10, width = 100)
-    requete.bind("<Leave>", requete_verifier)
-    requete.grid(row = 7, column = 0, columnspan = 2)
-
     # affichage en cas de problemes avec la requete
-    r_texte = Tk.StringVar()
-    erreur_requete = Tk.Label(root, text = "", fg = "red", textvariable = r_texte)
-    erreur_requete.grid(row = 8, column = 0, sticky = "w")
 
     # affichage d'un bouton de validation et de retour
-    Tk.Button(root, text = "valider", command = validation, width = 20, height = 5).grid(row = 9, column = 0, sticky = "w")
-    Tk.Button(root, text = "retour", command = retour, width = 20, height = 5).grid(row = 9, column = 1, sticky = "e")
-
 
     # on vérifie si une requete n'été pas déjà en cours, si oui, on la charge, sinon, on crée le fichier temp.txt
     if read.fichier_existe(requetes + "/temp.txt"):
         temporaire = read.lire_fichier(requetes + "/temp.txt", True)
 
-        numero.insert("current linestart", temporaire[0])
-        question_sql.insert("current linestart", temporaire[1])
-
-        req = ""
-        for ligne in temporaire[2:]:
-            req += ligne + "\n"
-
-        requete.insert("1.0", req)
-
-        numero_verifier("")
-        question_verifier("")
-        requete_verifier("")
     else:
         read.add_fichier(requetes, "temp.txt")
 
-    root.bind("<Escape>", retour)
+    # on met un raccourci clavier pour retourner au questions
     temp()
 
 def caracteres(texte):
@@ -330,6 +202,7 @@ def caracteres(texte):
 
     return lettres
 
+# c'est une sorte de div qui s'ajoute quand on clique sur le bouton
 def supprimer(root, dico, requetes, config, base):
     """
     fonction permettant d'ajouter une requête via une fenetre tkinter
@@ -340,9 +213,6 @@ def supprimer(root, dico, requetes, config, base):
         config, une chaine de caracteres avec le nom du fichier de configuration des requetes
         base, une chaine de caracteres avec le chemin d'accès vers la base de données
     """
-    # on nettoie la fenêtre et on change son nom
-    tk.clean(root, "Menu")
-    root.title("Suppression de requêtes")
     questions_supprimer = []
 
     def cases_cochees():
@@ -353,9 +223,10 @@ def supprimer(root, dico, requetes, config, base):
         """
         numeros_cases = []
 
-        for i in range(0,len(questions_supprimer)):
-            if questions_supprimer[i][0].get() == 1: # On regarde si la case à été coché
-                numeros_cases.append(questions_supprimer[i][1]) # on ajoute l'index de la question
+        for i in range(0,0): # nombre de questions possibles
+            pass
+            # On regarde si la case à été coché
+                # on ajoute l'index de la question
 
         return numeros_cases
 
@@ -387,7 +258,7 @@ def supprimer(root, dico, requetes, config, base):
                 i_lignes += [l]
                 l += 1
 
-                # on ajoute les indices tant qu'on n'as pas fini la question
+                # on ajoute les indices tant qu'on n'as pas fini de parcourir les lignes de la question
                 while l < len(alire) and len(alire[l]) > 0 and alire[l][0] != "#":
                     i_lignes += [l]
                     l += 1
@@ -396,36 +267,16 @@ def supprimer(root, dico, requetes, config, base):
 
             retour() # on retourne au menu
 
-    def retour(event = ""):
+    def retour():
         """
         sous-fonction permettant de revenir au menu principal
         elle fait redémarrer le programme
         """
-        question(root, requetes, config, base)
+        pass
 
-    root.bind("<Escape>", retour)
+    # ajout d'un racourci clavier pour retourner au question
 
-    # d'ici à la barre, on installe une scrollbar pour pouvoir afficher toutes les questions (cela évite les fenêtres trop grandes)
-    ff = Tk.Frame(root)
-    ff.grid(row=0, column=0, columnspan=2)
-
-    H = len(dico.values()) * 25
-    W = str( len_maximum([t[0] for t in dico.values()]) //5) + "c"
-
-    canvas = Tk.Canvas(ff, width=W, scrollregion =(0, 0, 10, H))
-    canvas.pack(side="left")
-
-    frame = Tk.Frame(canvas)
-    frame.pack()
-
-    vbar = Tk.Scrollbar(ff,orient="vertical")
-    vbar.pack(side="right", fill="y")
-    vbar.config(command=canvas.yview)
-    canvas['yscrollcommand']=vbar.set
-    canvas.create_window((0,0),window=frame,width=W, height=H, anchor='nw')
-    ############################################################################
-
-    # on ajoute les requêtes
+    # on affiche les requetes possibles
     ligne = 0
     for number, lien in dico.items(): # on parcours le dictionnaire qui contient toutes les questions
         valeur = Tk.IntVar()
@@ -439,8 +290,6 @@ def supprimer(root, dico, requetes, config, base):
         check.grid(row = ligne, column = 0, sticky="w")
 
     # affichage d'un bouton de validation et de retour
-    Tk.Button(root, text = "valider", command = validation, width = 20, height = 5).grid(row = 1, column = 0, sticky = "w")
-    Tk.Button(root, text = "retour", command = retour, width = 20, height = 5).grid(row = 1, column = 1, sticky = "e")
 
 
 def len_maximum(tab):
@@ -461,24 +310,41 @@ def len_maximum(tab):
 def aide():
     """
     fonction permettant d'effectuer l'affichage de l'aide
-    renvoie une fenetre tkinter avec l'aide
+    renvoie
     """
-    texte_liste = read.lire_fichier("README.md", True)
-    texte = ""
-    for t in texte_liste:
-        texte += t + "\n"
-    tk.affichage_texte_tkinter("aide", texte, len_maximum(texte_liste) +2)
+    if read.fichier_existe("data/aide.html"):
+        read.suppr_fichier("data/aide.html", False)
+
+    texte = "<html><head>\nAide\n</head><body><p>\n"
+
+    aide_contenu = read.lire_fichier("README.md", True)
+    for i in aide_contenu:
+        texte += "</br>\n"
+        texte += i + "\n"
+
+
+    texte += "</p></body></html>"
+
+    read.add_fichier("data", "aide.html", texte)
 
 def credit():
     """
     fonction permettant d'effectuer l'affichage des crédits
-    renvoie une fenetre tkinter avec les crédits
+    renvoie
     """
-    texte_liste = read.lire_fichier("credits.md", True)
-    texte = ""
-    for t in texte_liste:
-        texte += t + "\n"
-    tk.affichage_texte_tkinter("credits", texte, len_maximum(texte_liste) +2)
+    if read.fichier_existe("data/credits.html"):
+        read.suppr_fichier("data/credits.html", False)
+
+    texte = "<html><head>\nCrédits\n</head><body><p>\n"
+
+    aide_contenu = read.lire_fichier("credits.md", True)
+    for i in aide_contenu:
+        texte += "</br>\n"
+        texte += i + "\n"
+
+    texte += "</p></body></html>"
+
+    read.add_fichier("data", "credits.html", texte)
 
 def test():
     """
